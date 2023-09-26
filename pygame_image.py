29eb92226e448de7aed8bd8f536fa2096b51fad4
c0b22021 <c0b220219f@edu.teu.ac.jp>
2023-09-26 16:17:02 +0900

    en1

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,13 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     ton = pg.image.load("ex01/fig/3.png")
     ton = pg.transform.flip(ton, True, False)
-    ls = [ton, pg.transform.rotozoom(ton, 10, 1.0)]
+    ls = []
+    for i in range(20):
+        if i <= 10:
+            ls.append(pg.transform.rotozoom(ton, i, 1.0))
+        else:
+            ls.append(pg.transform.rotozoom(ton, (20-i), 1.0))
+    #ls = [ton, pg.transform.rotozoom(ton, 10, 1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -17,10 +23,10 @@ def main():
         x = tmr%1600
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [1600-x, 0])
-        screen.blit(ls[tmr%2], [300, 200])
+        screen.blit(ls[tmr%20], [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
